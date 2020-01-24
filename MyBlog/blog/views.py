7048cd46fe936afb114e9d5eb7fb5dae53b92d31
unknown <vladimirvoron7@gmail.com>
2020-01-24 16:59:35 +0300

    templates

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def posts_list(request):
-    return HttpResponse('<h1>Hi</h1>')
+    n = ['Alex','Vova','Wizard']
+    return render(request, 'blog/index.html', context={'names':n})
